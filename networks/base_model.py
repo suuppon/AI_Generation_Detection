87534ae03d8fa9e43b77abde63207728efb553b6
suuppon <albert1226@g.skku.edu>
2025-09-02 22:46:18 +0900

    feat: add multitower model

--- networks/base_model.py
+++ networks/base_model.py
@@ -3,74 +3,82 @@ import os
 import torch
 import torch.nn as nn
 from torch.nn import init
-from torch.optim import lr_scheduler
-
 
 class BaseModel(nn.Module):
+    """
+    self.model: 실제 분류기(혹은 멀티타워 래퍼)를 붙여서 사용
+    optimizer는 외부에서 붙일 수 있음. save/load 시 optimizer, total_steps도 함께 저장 가능.
+    """
     def __init__(self, opt):
-        super(BaseModel, self).__init__()
+        super().__init__()
         self.opt = opt
         self.total_steps = 0
         self.isTrain = opt.isTrain
         self.lr = opt.lr
         self.save_dir = os.path.join(opt.checkpoints_dir, opt.name)
-        self.device = torch.device('cuda:{}'.format(opt.gpu_ids[0])) if opt.gpu_ids else torch.device('cpu')
+        self.device = torch.device(f'cuda:{opt.gpu_ids[0]}') if opt.gpu_ids else torch.device('cpu')
+        self.model = None
+        self.optimizer = None
+
+    def attach(self, model: nn.Module, optimizer=None):
+        self.model = model.to(self.device)
+        self.optimizer = optimizer
 
     def save_networks(self, epoch):
-        save_filename = 'model_epoch_%s.pth' % epoch
+        os.makedirs(self.save_dir, exist_ok=True)
+        save_filename = f'model_epoch_{epoch}.pth'
         save_path = os.path.join(self.save_dir, save_filename)
 
-        # serialize model and optimizer to dict
-        # state_dict = {
-        #     'model': self.model.state_dict(),
-        #     'optimizer' : self.optimizer.state_dict(),
-        #     'total_steps' : self.total_steps,
-        # }
+        payload = {
+            "model": self.model.state_dict() if self.model is not None else None,
+            "optimizer": self.optimizer.state_dict() if (self.isTrain and self.optimizer is not None) else None,
+            "total_steps": self.total_steps,
+            "lr": self.lr,
+        }
+        torch.save(payload, save_path)
+        print(f'[BaseModel] Saved: {save_path}')
 
-        torch.save(self.model.state_dict(), save_path)
-        print(f'Saving model {save_path}')
-
-    # load models from the disk
     def load_networks(self, epoch):
-        load_filename = 'model_epoch_%s.pth' % epoch
+        load_filename = f'model_epoch_{epoch}.pth'
         load_path = os.path.join(self.save_dir, load_filename)
+        print(f'[BaseModel] Loading from {load_path}')
+        state = torch.load(load_path, map_location=self.device)
 
-        print('loading the model from %s' % load_path)
-        # if you are using PyTorch newer than 0.4 (e.g., built from
-        # GitHub source), you can remove str() on self.device
-        state_dict = torch.load(load_path, map_location=self.device)
-        if hasattr(state_dict, '_metadata'):
-            del state_dict._metadata
+        # 두 형태 모두 지원: 1) payload dict  2) 바로 state_dict
+        if isinstance(state, dict) and "model" in state:
+            self.model.load_state_dict(state["model"])
+            self.total_steps = state.get("total_steps", 0)
+            self.lr = state.get("lr", self.opt.lr)
+            if self.isTrain and (not getattr(self.opt, "new_optim", False)) and self.optimizer is not None:
+                if state.get("optimizer") is not None:
+                    self.optimizer.load_state_dict(state["optimizer"])
+                    # 옵티마 state를 GPU로
+                    for g in self.optimizer.param_groups:
+                        g['lr'] = self.opt.lr
+                    for s in self.optimizer.state.values():
+                        for k, v in s.items():
+                            if torch.is_tensor(v):
+                                s[k] = v.to(self.device)
+        else:
+            # 과거 형식: 모델 state_dict만 저장된 경우
+            self.model.load_state_dict(state)
 
-        self.model.load_state_dict(state_dict['model'])
-        self.total_steps = state_dict['total_steps']
-
-        if self.isTrain and not self.opt.new_optim:
-            self.optimizer.load_state_dict(state_dict['optimizer'])
-            ### move optimizer state to GPU
-            for state in self.optimizer.state.values():
-                for k, v in state.items():
-                    if torch.is_tensor(v):
-                        state[k] = v.to(self.device)
-
-            for g in self.optimizer.param_groups:
-                g['lr'] = self.opt.lr
-
-    def eval(self):
+    def eval(self):  # noqa: A003
         self.model.eval()
 
-    def train(self):
+    def train(self):  # noqa: A003
         self.model.train()
 
-    def test(self):
-        with torch.no_grad():
-            self.forward()
+    @torch.no_grad()
+    def test(self, *args, **kwargs):
+        self.model.eval()
+        return self.model(*args, **kwargs)
 
 
 def init_weights(net, init_type='normal', gain=0.02):
     def init_func(m):
         classname = m.__class__.__name__
-        if hasattr(m, 'weight') and (classname.find('Conv') != -1 or classname.find('Linear') != -1):
+        if hasattr(m, 'weight') and ('Conv' in classname or 'Linear' in classname):
             if init_type == 'normal':
                 init.normal_(m.weight.data, 0.0, gain)
             elif init_type == 'xavier':
@@ -80,12 +88,11 @@ def init_weights(net, init_type='normal', gain=0.02):
             elif init_type == 'orthogonal':
                 init.orthogonal_(m.weight.data, gain=gain)
             else:
-                raise NotImplementedError('initialization method [%s] is not implemented' % init_type)
-            if hasattr(m, 'bias') and m.bias is not None:
+                raise NotImplementedError(f'init method [{init_type}] not implemented')
+            if getattr(m, 'bias', None) is not None:
                 init.constant_(m.bias.data, 0.0)
-        elif classname.find('BatchNorm2d') != -1:
+        elif 'BatchNorm2d' in classname:
             init.normal_(m.weight.data, 1.0, gain)
             init.constant_(m.bias.data, 0.0)
-
-    print('initialize network with %s' % init_type)
+    print(f'initialize network with {init_type}')
     net.apply(init_func)
--- train.py
+++ train.py
@@ -1,111 +1,253 @@
-import os
-import sys
-import time
+# # train.py (main)
+# import os
+# import sys
+# import time
+# import torch
+# import argparse
+# from PIL import Image
+# from tensorboardX import SummaryWriter
+# import numpy as np
+
+# from validate import validate
+# from data import create_dataloader
+# from utils.trainer import Trainer          # ★ 경로 수정
+# from options.train_options import TrainOptions
+# from options.test_options import TestOptions
+# from util import Logger
+
+# import random
+
+# def seed_torch(seed=1029):
+#     random.seed(seed)
+#     os.environ['PYTHONHASHSEED'] = str(seed)
+#     np.random.seed(seed)
+#     torch.manual_seed(seed)
+#     torch.cuda.manual_seed(seed)
+#     torch.cuda.manual_seed_all(seed)  # multi-GPU
+#     torch.backends.cudnn.benchmark = False
+#     torch.backends.cudnn.deterministic = True
+#     torch.backends.cudnn.enabled = False
+
+# # test config
+# vals = ['progan', 'stylegan', 'stylegan2', 'biggan', 'cyclegan', 'stargan', 'gaugan', 'deepfake']
+# multiclass = [1, 1, 1, 0, 1, 0, 0, 0]
+
+# def get_val_opt(base_opt):
+#     # base_opt를 카피해서 val 세팅만 변경
+#     val_opt = TrainOptions().parse(print_options=False)
+#     # 학습 파이프 옵션이 val에 영향을 주지 않도록 최소만 세팅
+#     val_opt.dataroot = f'{base_opt.dataroot_root}/{base_opt.val_split}/'
+#     val_opt.isTrain = False
+#     val_opt.no_resize = False
+#     val_opt.no_crop = False
+#     val_opt.serial_batches = True
+#     return val_opt
+
+# if __name__ == '__main__':
+#     opt = TrainOptions().parse()
+#     seed_torch(100)
+
+#     # 학습/검증 경로 정리
+#     opt.dataroot_root = opt.dataroot.rstrip('/')
+
+#     Testdataroot = os.path.join(opt.dataroot_root, 'test')
+#     opt.dataroot = f'{opt.dataroot_root}/{opt.train_split}/'
+
+#     Logger(os.path.join(opt.checkpoints_dir, opt.name, 'log.log'))
+#     print('  '.join(list(sys.argv)) )
+
+#     # dict 배치 파이프를 기본값으로 권장 (옵션에서 변경했다면 그대로 사용)
+#     if not hasattr(opt, 'proc_mode') or not opt.proc_mode:
+#         opt.proc_mode = 'dict'
+#     if not hasattr(opt, 'features') or not opt.features:
+#         # 최소 1개 feature 필요. 기존 전처리와 가장 가까운 edge+texture 권장
+#         opt.features = ['edge', 'texture']
+
+#     # DataLoader
+#     data_loader = create_dataloader(opt)
+
+#     # SummaryWriter
+#     train_writer = SummaryWriter(os.path.join(opt.checkpoints_dir, opt.name, "train"))
+#     val_writer = SummaryWriter(os.path.join(opt.checkpoints_dir, opt.name, "val"))
+
+#     # Trainer
+#     model = Trainer(opt)
+
+#     # ---- Validation Option (val split) ----
+#     val_opt = get_val_opt(opt)
+
+#     # ---- 테스트 셋 루틴 ----
+#     Testopt = TestOptions().parse(print_options=False)
+
+#     def testmodel():
+#         print('*'*25)
+#         accs = []; aps = []
+#         print(time.strftime("%Y_%m_%d_%H_%M_%S", time.localtime()))
+#         for v_id, val in enumerate(vals):
+#             Testopt.dataroot = f'{Testdataroot}/{val}'
+#             Testopt.classes = os.listdir(Testopt.dataroot) if multiclass[v_id] else ['']
+#             Testopt.no_resize = False
+#             Testopt.no_crop = True
+#             # validate가 4D 입력(B,C,H,W) 기반이면, MultiTowerFromCloned도 4D를 지원
+#             acc, ap, _, _, _, _ = validate(model.model, Testopt)
+#             accs.append(acc); aps.append(ap)
+#             print(f"({v_id} {val:10}) acc: {acc*100:.1f}; ap: {ap*100:.1f}")
+#         print(f"({v_id+1} {'Mean':10}) acc: {np.mean(accs)*100:.1f}; ap: {np.mean(aps)*100:.1f}")
+#         print('*'*25)
+#         print(time.strftime("%Y_%m_%d_%H_%M_%S", time.localtime()))
+
+#     # 사전 점검용 테스트 1회
+#     model.eval(); testmodel(); model.train()
+
+#     print(f'cwd: {os.getcwd()}')
+#     for epoch in range(opt.niter):
+#         epoch_start_time = time.time()
+#         epoch_iter = 0
+
+#         for i, data in enumerate(data_loader):
+#             model.total_steps += 1
+#             epoch_iter += opt.batch_size
+
+#             # data: (x_dict_or_list, labels)  — dataloader가 dict 모드면 dict가 들어옴
+#             model.set_input(data)
+#             model.optimize_parameters()
+
+#             if model.total_steps % opt.loss_freq == 0:
+#                 lr_now = model.optimizer.param_groups[0]['lr']
+#                 print(time.strftime("%Y_%m_%d_%H_%M_%S", time.localtime()),
+#                       f"Train loss: {model.loss:.6f} at step: {model.total_steps} lr {lr_now:g}")
+#                 train_writer.add_scalar('loss', float(model.loss), model.total_steps)
+#                 train_writer.add_scalar('lr', float(lr_now), model.total_steps)
+
+#         # 스케줄링
+#         if epoch % opt.delr_freq == 0 and epoch != 0:
+#             print(time.strftime("%Y_%m_%d_%H_%M_%S", time.localtime()),
+#                   f'changing lr at the end of epoch {epoch}, iters {model.total_steps}')
+#             model.adjust_learning_rate()
+
+#         # Validation (val split)
+#         model.eval()
+#         acc, ap = validate(model.model, val_opt)[:2]
+#         val_writer.add_scalar('accuracy', float(acc), model.total_steps)
+#         val_writer.add_scalar('ap', float(ap), model.total_steps)
+#         print(f"(Val @ epoch {epoch}) acc: {acc}; ap: {ap}")
+
+#         # 추가 테스트셋 루틴
+#         testmodel()
+#         model.train()
+
+#     model.eval(); testmodel()
+#     model.save_networks('last')
+
+# train.py
+import os, sys, time, random
 import torch
-import torch.nn
-import argparse
-from PIL import Image
-from tensorboardX import SummaryWriter
 import numpy as np
-from validate import validate
+from tensorboardX import SummaryWriter
+from tqdm.auto import tqdm
+
 from data import create_dataloader
-from networks.trainer import Trainer
+from utils.trainer import Trainer
 from options.train_options import TrainOptions
-from options.test_options import TestOptions
 from util import Logger
 
-import random
 def seed_torch(seed=1029):
     random.seed(seed)
     os.environ['PYTHONHASHSEED'] = str(seed)
     np.random.seed(seed)
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
-    torch.cuda.manual_seed_all(seed) # if you are using multi-GPU.
+    torch.cuda.manual_seed_all(seed)
     torch.backends.cudnn.benchmark = False
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.enabled = False
 
-
-# test config
-vals = ['progan', 'stylegan', 'stylegan2', 'biggan', 'cyclegan', 'stargan', 'gaugan', 'deepfake']
-multiclass = [1, 1, 1, 0, 1, 0, 0, 0]
-
-
-def get_val_opt():
-    val_opt = TrainOptions().parse(print_options=False)
-    val_opt.dataroot = '{}/{}/'.format(val_opt.dataroot, val_opt.val_split)
-    val_opt.isTrain = False
-    val_opt.no_resize = False
-    val_opt.no_crop = False
-    val_opt.serial_batches = True
-
-    return val_opt
-
-
 if __name__ == '__main__':
     opt = TrainOptions().parse()
     seed_torch(100)
-    Testdataroot = os.path.join(opt.dataroot, 'test')
-    opt.dataroot = '{}/{}/'.format(opt.dataroot, opt.train_split)
+
+    # dataroot는 images 루트로 바로 지정 (예: ./images)
+    # ex) python train.py --dataroot ./images
+    opt.isTrain = True
+    if not hasattr(opt, 'proc_mode') or not opt.proc_mode:
+        opt.proc_mode = 'dict'
+    if not hasattr(opt, 'features') or not opt.features:
+        opt.features = ['edge','texture']
+
     Logger(os.path.join(opt.checkpoints_dir, opt.name, 'log.log'))
-    print('  '.join(list(sys.argv)) )
-    val_opt = get_val_opt()
-    Testopt = TestOptions().parse(print_options=False)
+    print('  '.join(list(sys.argv)))
+
+    # DataLoader (images/{real,fake})
+    # TODO : CHECK
     data_loader = create_dataloader(opt)
 
+    # Writers
     train_writer = SummaryWriter(os.path.join(opt.checkpoints_dir, opt.name, "train"))
-    val_writer = SummaryWriter(os.path.join(opt.checkpoints_dir, opt.name, "val"))
-    
+
+    # Model
+    # TODO : CHECK
     model = Trainer(opt)
-    
-    def testmodel():
-        print('*'*25);accs = [];aps = []
-        print(time.strftime("%Y_%m_%d_%H_%M_%S", time.localtime()))
-        for v_id, val in enumerate(vals):
-            Testopt.dataroot = '{}/{}'.format(Testdataroot, val)
-            Testopt.classes = os.listdir(Testopt.dataroot) if multiclass[v_id] else ['']
-            Testopt.no_resize = False
-            Testopt.no_crop = True
-            acc, ap, _, _, _, _ = validate(model.model, Testopt)
-            accs.append(acc);aps.append(ap)
-            print("({} {:10}) acc: {:.1f}; ap: {:.1f}".format(v_id, val, acc*100, ap*100))
-        print("({} {:10}) acc: {:.1f}; ap: {:.1f}".format(v_id+1,'Mean', np.array(accs).mean()*100, np.array(aps).mean()*100));print('*'*25) 
-        print(time.strftime("%Y_%m_%d_%H_%M_%S", time.localtime()))
-    model.eval();testmodel();
     model.train()
     print(f'cwd: {os.getcwd()}')
+
     for epoch in range(opt.niter):
         epoch_start_time = time.time()
-        iter_data_time = time.time()
-        epoch_iter = 0
+        running_loss = 0.0
+        num_steps = 0
 
-        for i, data in enumerate(data_loader):
+        # tqdm 진행바: 에폭/총에폭, 현재 LR, 최근 loss 표시
+        pbar = tqdm(
+            data_loader,
+            desc=f"Epoch {epoch+1}/{opt.niter}",
+            dynamic_ncols=True,
+            leave=False
+        )
+
+        for i, data in enumerate(pbar):
             model.total_steps += 1
-            epoch_iter += opt.batch_size
-
+            
+            # TODO : CHECK
             model.set_input(data)
             model.optimize_parameters()
 
+            # 통계/표시용
+            loss_val = float(model.loss)
+            running_loss += loss_val
+            num_steps += 1
+            lr_now = model.optimizer.param_groups[0]['lr']
+
+            # 진행바 우측 postfix 업데이트
+            pbar.set_postfix({
+                "loss": f"{loss_val:.4f}",
+                "lr": f"{lr_now:.2e}",
+                "step": model.total_steps
+            })
+
+            # 텐서보드 로깅(기존 주기 유지)
             if model.total_steps % opt.loss_freq == 0:
-                print(time.strftime("%Y_%m_%d_%H_%M_%S", time.localtime()), "Train loss: {} at step: {} lr {}".format(model.loss, model.total_steps, model.lr))
-                train_writer.add_scalar('loss', model.loss, model.total_steps)
+                print(
+                    time.strftime("%Y_%m_%d_%H_%M_%S", time.localtime()),
+                    f"Train loss: {loss_val:.6f} at step: {model.total_steps} lr {lr_now:g}"
+                )
+                train_writer.add_scalar('loss', loss_val, model.total_steps)
+                train_writer.add_scalar('lr', float(lr_now), model.total_steps)
 
+        # 에폭 끝나고 평균 loss 간단 출력
+        if num_steps > 0:
+            avg_loss = running_loss / num_steps
+            print(
+                time.strftime("%Y_%m_%d_%H_%M_%S", time.localtime()),
+                f"[Epoch {epoch+1}/{opt.niter}] avg_loss: {avg_loss:.6f}, steps: {model.total_steps}"
+            )
+
+        # 주기적 LR decay
         if epoch % opt.delr_freq == 0 and epoch != 0:
-            print(time.strftime("%Y_%m_%d_%H_%M_%S", time.localtime()), 'changing lr at the end of epoch %d, iters %d' %
-                  (epoch, model.total_steps))
+            print(
+                time.strftime("%Y_%m_%d_%H_%M_%S", time.localtime()),
+                f'changing lr at the end of epoch {epoch}, iters {model.total_steps}'
+            )
             model.adjust_learning_rate()
-            
 
-        # Validation
-        model.eval()
-        acc, ap = validate(model.model, val_opt)[:2]
-        val_writer.add_scalar('accuracy', acc, model.total_steps)
-        val_writer.add_scalar('ap', ap, model.total_steps)
-        print("(Val @ epoch {}) acc: {}; ap: {}".format(epoch, acc, ap))
-        testmodel()
-        model.train()
-
-    model.eval();testmodel()
+    # 마지막 저장
+    model.eval()
     model.save_networks('last')
-    
